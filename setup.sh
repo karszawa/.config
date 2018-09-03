15141c909cedf7706360ad07c52b44c8c93307da
Hiroaki KARASAWA <siqvare@gmail.com>
2018-09-04 03:31:43 +0900

    Implement echo_step

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 # docker
 # .gitconfig
 
+declare step_counter=1
+function echo_step {
+  echo -e "\033[34m\033[1mSTEP #$step_counter: $1\033[0m"
+  let ++step_counter
+}
+
 cd ~
 
-echo '# SET UP homebrew'
+echo_step 'homebrew'
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -13,19 +19,19 @@ hub clone --depth=1 https://github.com/karszawa/.config
 
 brew bundle install --file=~/.config/Brewfile
 
-echo '# SET UP git'
+echo_step 'git'
 
 ln -s ~/.config/.gitconfig ~/.gitconfig
 ln -s ~/.config/.gitattributes ~/.gitattributes
 ln -s ~/.config/.gitignore-global ~/.gitignore-global
 
-echo '# SET UP fish'
+echo_step 'fish'
 
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
 
 fisher
 
-echo '# SET UP vim'
+echo_step 'vim'
 
 ln -s .vim/ ../.vim
 
@@ -34,7 +40,7 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 git clone --depth=1 https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 
-echo '# SET UP visual-studio-code'
+echo_step 'Visual Studio Code'
 
 ln -s ~/.config/code/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s ~/.config/code/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
@@ -42,13 +48,13 @@ ln -s ~/.config/code/locale.json ~/Library/Application\ Support/Code/User/locale
 
 cat ~/.config/.vscode/extensions.list | xargs -L1 code --install-extension
 
-echo '# SET UP google-cloud-sdk'
+echo_step 'google-cloud-sdk'
 
 curl https://sdk.cloud.google.com | bash
 
 chsh -s /usr/local/bin/fish
 
-echo '# SET UP iTerm2'
+echo_step 'iTerm2'
 
 echo 'Open iTerm2 and check "Load preferences from a custom folder or URL" then fill text box with "~/.config/iterm2"'
 
