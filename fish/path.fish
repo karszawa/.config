3e87734c665e631ba84482adcfc182282fb2427f
Hiroaki KARASAWA <siqvare@gmail.com>
2018-09-03 20:36:05 +0900

    Modify fish path

--- fish/path.fish
+++ fish/path.fish
@@ -1,10 +1,17 @@
-set -gx PATH /usr/local/bin $PATH # Homebrew
-set -gx PATH $HOME/.nodebrew/current/bin $PATH # nodebrew
-set -gx PATH $PATH /usr/local/share/git-core/contrib/diff-highlight # for git diff-highlight
-set -gx PATH $PATH ~/.cargo/bin
-set -gx LLVM_CONFIG /usr/local/Cellar/llvm/5.0.0/bin/llvm-config
+if [ -f '~/dev' ]
+  mkdir '~/dev'
+end
+
 set -gx GOPATH ~/dev
-set -gx PATH $PATH $GOPATH/bin
-set -gx PATH $PATH $PATH ~/Library/Android/sdk/platform-tools
-set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
-set -gx PATH $PATH '/Applications/Visual Studio Code.app/Contents/Resources/app/bin'
+
+set paths \
+  $GOPATH/bin \
+  ~/.cargo/bin \
+  '/usr/local/share/git-core/contrib/diff-highlight' \
+  '/Applications/Visual Studio Code.app/Contents/Resources/app/bin' \
+
+for path in $paths
+  if not [ -f $path ]
+    set -gx fish_user_paths $path $fish_user_paths
+  end
+end
