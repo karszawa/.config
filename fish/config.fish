92cd6cf79c6d06561beb786a37b5cf3ab752a07e
Hiroaki KARASAWA <siqvare@gmail.com>
2019-04-14 22:55:52 +0900

    Update

--- fish/config.fish
+++ fish/config.fish
@@ -3,4 +3,15 @@ source ~/.config/fish/prompt.fish
 source ~/.config/fish/alias.fish
 source ~/.config/fish/keybindings.fish
 
+export GPG_TTY=(tty)
+export GO111MODULE=on
+
+# https://qiita.com/delphinus/items/b04752bb5b64e6cc4ea9
+export LESS='-g -i -M -R -S -W -z-4 -x4'
+export PAGER=less
+
+if which lesspipe.sh > /dev/null
+  export LESSOPEN='| /usr/bin/env lesspipe.sh %s 2>&-'
+end
+
 set -U EDITOR vim
