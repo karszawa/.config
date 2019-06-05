f4928dfeb229f39e6318c3f55b188b0d04253e0c
Hiroaki KARASAWA <karszawa@gmail.com>
2019-06-05 10:48:58 +0900

    Change emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,9 @@
 (keyboard-translate ?\C-h ?\C-?)
 
 (menu-bar-mode -1)
+
+; stop creating backup~ files
+(setq make-backup-files nil)
+
+; stop creating #autosave# files
+(setq auto-save-default nil)
